07cdc54b204c87a493884c15de948c0be1c5b56f
nechamablerner <nechamablerner731@gmail.com>
2026-05-05 01:17:45 -0400

    fix import

--- src/ProductDetails.tsx
+++ src/ProductDetails.tsx
@@ -20,7 +20,7 @@ function ProductDetails(){
         <>
         <main className={styles.container}>
             <div className={styles.imageSelection}>
-                <img src={`images/${product.image}`} alt={product.name} className={styles.img} />
+                <img src={`${import.meta.env.BASE_URL}images/${product.image}`} />
             </div>
 
             <div className={styles.infoSection}>
